e6e3103d15d79803d2e2691c589911298cfd7e74
Saúl Sánchez <link_mx@live.com>
2023-05-18 11:30:42 -0600

    Revert "Feat/input text"

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
-<script setup lang="ts">
-import HomeView from '@/views/HomeView.vue';
-import TheNavBar from '@/components/TheNavBar.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="lp">
-        <TheNavBar />
-        <HomeView />
-    </div>
+  <div class="lp">
+    <header class="p-1">
+      UX/UI Challenge
+    </header>
+    <main class="p-2">
+      Test content with loan pro
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
