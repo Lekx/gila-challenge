a9ee9d249df78d9aa874fac64a34c7793821a21a
Saúl Sánchez <link_mx@live.com>
2023-05-18 11:27:48 -0600

    InputText completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'standard-with-typescript',
+    '@vue/typescript/recommended',
     'prettier'
   ],
   overrides: [
